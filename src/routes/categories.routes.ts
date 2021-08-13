51738af2d9693b981c157e7a00a16f8f31b709d4
Nico Serrato <nicolas.serrato@outlook.com>
2021-08-13 16:20:11 -0300

    WIP adds file upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,28 @@
 import { Router } from "express";
+import multer  from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
+
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);  
 });
 
 categoriesRoutes.get("/", (request, response) => {
   listCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file)
+
+  return response.send();
 });
 
 export { categoriesRoutes };
